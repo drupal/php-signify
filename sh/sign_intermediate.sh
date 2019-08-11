c7c1edff1ba067059a1c40f5bdd81c4e72fccdc7
David Strauss <david@davidstrauss.net>
2019-08-11 14:34:13 -0400

    Add script for root key to sign intermediate

---
+++ sh/sign_intermediate.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+rootseckey=$1
+intpubkey=$2
+>&2 echo Signing intermediate public key $2 with root secret $1
+msgfile=$(mktemp /tmp/intsigmsg.XXXXXX)
+date --utc --iso-8601 --date="+30 days" > $msgfile
+cat $intpubkey >> $msgfile
+signify -S -e -s $rootseckey -m $msgfile -x -
+rm "$msgfile"
